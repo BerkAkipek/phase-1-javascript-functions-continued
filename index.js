a6a7a191f82c1ccfedebb521ac934efb9a1bdc3e
BerkAkipek <berk.akipek.96@gmail.com>
2022-05-31 17:31:16 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+// I never imagined scope more then 2Dimensional. Space-time magic!
+
+const wrapAdjective = (highLight="*") => {
+    return function (compliment="special") {
+        return `You are ${highLight}${compliment}${highLight}!`
+    }
+}
+
